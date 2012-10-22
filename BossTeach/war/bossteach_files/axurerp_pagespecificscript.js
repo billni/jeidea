a0d7492412537dce5fb4c307fc9f2041ba04d782
niyong <niyong@ulic-niyong.mshome.net>
2012-10-22 17:32:52 +0800

    create

---
+++ BossTeach/war/bossteach_files/axurerp_pagespecificscript.js
@@ -0,0 +1,18 @@
+for(var i = 0; i < 29; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+document.getElementById('u17_img').tabIndex = 0;
+
+u17.style.cursor = 'pointer';
+$axure.eventManager.click('u17', function(e) {
+
+if (true) {
+
+	NewTab('mailto:bossteach2012@gmail.com', "");
+
+}
+});
+gv_vAlignTable['u28'] = 'top';gv_vAlignTable['u8'] = 'center';gv_vAlignTable['u6'] = 'center';gv_vAlignTable['u15'] = 'top';gv_vAlignTable['u14'] = 'center';gv_vAlignTable['u4'] = 'center';gv_vAlignTable['u1'] = 'top';gv_vAlignTable['u26'] = 'top';gv_vAlignTable['u10'] = 'center';gv_vAlignTable['u12'] = 'center';gv_vAlignTable['u24'] = 'top';gv_vAlignTable['u18'] = 'top';gv_vAlignTable['u20'] = 'top';gv_vAlignTable['u22'] = 'top';gv_vAlignTable['u0'] = 'top';
