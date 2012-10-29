b2cfb8e773c1b7b4865190766fb3f1c893715031
niyong <niyong@ulic-niyong.mshome.net>
2012-10-29 13:10:44 +0800

    add spring and struts2 lib jar

--- BossTeach/war/bossteach_files/axurerp_pagespecificscript.js
+++ BossTeach/war/bossteach_files/axurerp_pagespecificscript.js
@@ -1,13 +1,13 @@
-for(var i = 0; i < 29; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+for(var i = 0; i < 32; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
 
 $axure.eventManager.pageLoad(
 function (e) {
 
 });
-document.getElementById('u17_img').tabIndex = 0;
+gv_vAlignTable['u31'] = 'center';gv_vAlignTable['u17'] = 'center';gv_vAlignTable['u28'] = 'top';gv_vAlignTable['u21'] = 'top';gv_vAlignTable['u6'] = 'top';gv_vAlignTable['u15'] = 'center';gv_vAlignTable['u13'] = 'center';gv_vAlignTable['u4'] = 'top';gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u27'] = 'top';gv_vAlignTable['u23'] = 'top';gv_vAlignTable['u25'] = 'top';gv_vAlignTable['u2'] = 'top';gv_vAlignTable['u18'] = 'top';document.getElementById('u20_img').tabIndex = 0;
 
-u17.style.cursor = 'pointer';
-$axure.eventManager.click('u17', function(e) {
+u20.style.cursor = 'pointer';
+$axure.eventManager.click('u20', function(e) {
 
 if (true) {
 
@@ -15,4 +15,3 @@ if (true) {
 
 }
 });
-gv_vAlignTable['u28'] = 'top';gv_vAlignTable['u8'] = 'center';gv_vAlignTable['u6'] = 'center';gv_vAlignTable['u15'] = 'top';gv_vAlignTable['u14'] = 'center';gv_vAlignTable['u4'] = 'center';gv_vAlignTable['u1'] = 'top';gv_vAlignTable['u26'] = 'top';gv_vAlignTable['u10'] = 'center';gv_vAlignTable['u12'] = 'center';gv_vAlignTable['u24'] = 'top';gv_vAlignTable['u18'] = 'top';gv_vAlignTable['u20'] = 'top';gv_vAlignTable['u22'] = 'top';gv_vAlignTable['u0'] = 'top';
