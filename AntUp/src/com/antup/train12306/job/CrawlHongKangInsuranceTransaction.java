576b2598fcb99f80a42297a94d55fdffd3669d5f
billni <XJ@XJ-PC>
2013-05-26 12:32:24 +0800

    create antup

---
+++ AntUp/src/com/antup/train12306/job/CrawlHongKangInsuranceTransaction.java
@@ -0,0 +1,193 @@
+package com.antup.train12306.job;
+
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.NTCredentials;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.PoolingClientConnectionManager;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+public class CrawlHongKangInsuranceTransaction  {
+	private Log logger = LogFactory.getLog(CrawlHongKangInsuranceTransaction.class);
+	private static final String PROXY_HOST= "10.18.8.108";
+	private static final int PROXY_PORT = 8008;
+	private static final String PROXY_USERNAME= "niyong";
+	private static final String PROXY_PASSWORD= "nY111111";
+	private static final String PROXY_WORKSTATION= "isa06";
+	private static final String PROXY_DOMAIN= "ulic";
+	private static Long PageCount=0l;
+	private static String HongKangTransactionUrl = "http://baoxian.taobao.com/json/PurchaseList.do";
+	
+    public DefaultHttpClient getHttpClient(DefaultHttpClient httpClient){
+        NTCredentials credentials = new NTCredentials(PROXY_USERNAME ,PROXY_PASSWORD , PROXY_WORKSTATION, PROXY_DOMAIN);	        
+        httpClient.getCredentialsProvider().setCredentials(new AuthScope(PROXY_HOST, PROXY_PORT), credentials);	      
+        HttpHost proxy = new HttpHost(PROXY_HOST, PROXY_PORT);
+        httpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
+        return httpClient;
+    }	
+	
+	/** 
+	     * 根据URL获得html信息 
+	     * @param url 
+	     * @return 
+	     */  
+	    public String getHtmlByUrl(String url){
+	        StringWriter sw = new StringWriter();
+	        
+//	        DefaultHttpClient httpClient = getHttpClient(new DefaultHttpClient());//创建httpClient对象                	   
+	        DefaultHttpClient httpClient = new DefaultHttpClient();
+	        //如果代理�?��密码验证，这里设置用户名密码  	        
+	        HttpGet httpGet = new HttpGet(url);	        
+	        try {	        	
+	            HttpResponse response = httpClient.execute(httpGet);//得到responce对象  
+	            int resStatu = response.getStatusLine().getStatusCode();//返回 	 
+	            if (resStatu == HttpStatus.SC_OK) {//200正常
+	                //获得相应实体  	            	
+	                HttpEntity entity = response.getEntity();
+	                if (entity!=null) {	             
+	                	InputStreamReader insr = new InputStreamReader(entity.getContent(), "gb2312" /*ContentType.getOrDefault(entity).getCharset()*/);
+                        IOUtils.copy(insr, sw);
+	                	insr.close();
+	                }
+	            } else {
+		            System.out.println("Http Status Code:" + resStatu);
+	            }
+	        } catch (Exception e) {  
+	        	 System.out.println("Show exception when access this ulr.");
+	             e.printStackTrace();  
+	        } finally {	        	        	
+	        	logger.info("HttpClient连接关闭.");
+	            httpClient.getConnectionManager().shutdown();	            
+	        }  
+	        return sw.toString();  
+	    }
+	    
+	    public void analyseTransactionRecordsHtml(String html){
+//	    	HongKangInsuranceTransaction tbt = null;
+	        if (html!= null && !"".equals(html)) {	        	
+	            Document doc = Jsoup.parse(html);  
+	            Elements trs = doc.select("tbody tr");
+	            for (Element tr: trs) {
+	            	tr = tr.html(tr.html().replace("\\r", "").replace("\\t", "").replace("\\n", "").replace("\\/td", "").replace("&lt;", "").replace("&gt;", "").replace("\\/tr", "").replace(" <em>", "").replace("\\/em", "").replace("</em>", "").replace("\\/table", ""));
+//	            	tbt = new HongKangInsuranceTransaction();
+	            	//去掉第一�?
+	            	if ("".equals(tr.select("td:eq(0)").html())) {
+	            		continue;
+	            	}
+	            	//过滤不是今天的记�?
+	            	String currentDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+	            	if (!currentDate.equals(tr.select("td:eq(4)").html().subSequence(0, 10))) {
+	            		break;
+	            	}
+	            	System.out.println(tr.html());
+//	            	tbt.setSellerId("1128953583");
+//	            	tbt.setItemId("17305541936");
+//	            	tbt.setBuyer(tr.select("td:eq(0)").html());
+//	            	tbt.setItemName(tr.select("td:eq(1)").html());
+//	            	tbt.setPremium(Double.parseDouble(tr.select("td:eq(2)").html()));
+//	            	tbt.setCount(Long.parseLong(tr.select("td:eq(3)").html()));
+//	            	tbt.setTransactionDate(tr.select("td:eq(4)").html());
+//	            	tbt.setStatus(tr.select("td:eq(5)").html());
+//	            	taoBaoTransactionService.createHongKangInsuranceTransaction(tbt);
+	            }  
+	        } 
+	    }
+	    
+	    public void analyseTransactionCountHtml(String html){
+	        if (html!= null && !"".equals(html)) {	        	
+	            Document doc = Jsoup.parse(html);  
+	            Elements trs = doc.select("ul.tab-bar  li:not(.sel)");
+	            Pattern p = Pattern.compile("m>(.*)");
+	            Matcher m = p.matcher(trs.html());
+	            if (m.find()){
+		            PageCount = Long.parseLong(m.group().replace("m>", "").replace(" ", ""))/10;
+//		            System.out.println(m.group().replace("m>", "").replace("�?, "") + "�?�? + PageCount + "�?);
+	            }
+	        } 
+	    }
+	    
+	    
+	    public void pooledGetHtmlByUrl() throws Exception{
+	    	SchemeRegistry schemeRegistry = new SchemeRegistry();
+	    	schemeRegistry.register(new Scheme("http", 80, PlainSocketFactory.getSocketFactory()));
+	    	ClientConnectionManager cm = new PoolingClientConnectionManager(schemeRegistry);
+//	    	DefaultHttpClient httpClient = getHttpClient(new DefaultHttpClient(cm));
+	    	DefaultHttpClient httpClient = new DefaultHttpClient(cm);
+	    	
+	    	// URIs to perform GETs on
+	    	String[] urisToGet = {
+	    	   
+	    	};
+
+	    	// create a thread for each URI
+	    	GetThread[] threads = new GetThread[urisToGet.length];
+	    	for (int i = 0; i < threads.length; i++) {
+	    	    HttpGet httpGet = new HttpGet(urisToGet[i]);
+	    	    threads[i] = new GetThread(httpClient, httpGet);
+	    	}
+
+	    	// start the threads
+	    	for (int j = 0; j < threads.length; j++) {
+	    	    threads[j].start();
+	    	}
+
+	    	// join the threads
+	    	for (int j = 0; j < threads.length; j++) {
+	    	    threads[j].join();
+	    	}
+	    }
+	    
+	    public URI initUrl(){
+	    	URIBuilder builder;
+	    	URI ret = null;
+			try {
+				builder = new URIBuilder(HongKangTransactionUrl);
+				ret =  builder.addParameter("page", "1")
+ 	           .addParameter("itemid", "17305541936")
+ 	           .addParameter("sellerId", "1128953583")
+ 	           .addParameter("callback", "mycallback")
+ 	           .addParameter("sold_total_num","0")
+ 	          .addParameter("callback", "mycallback").build();
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+			}
+			return ret;
+	    }
+	    public static void main(String[] args) throws Exception {
+	    	CrawlHongKangInsuranceTransaction job = new CrawlHongKangInsuranceTransaction();
+	    	System.out.println(job.initUrl());
+	    	String html = job.getHtmlByUrl("http://baoxian.taobao.com/item.htm?spm=a220m.1000858.1000725.1.lMZiU8&id=17305541936&is_b=1&cat_id=2&q=%BA%EB%BF%B5&rn=859d895e481ccf0569738ec7a55d28a3");
+//	    	System.out.println(html);
+	    	job.analyseTransactionCountHtml(html);
+//	    	String html = job.getHtmlByUrl("http://baoxian.taobao.com/json/PurchaseList.do?page=1&itemId=17305541936&sellerId=1128953583&callback=mycallback&sold_total_num=0");
+//	    	job.analyseTransactionRecordsHtml(html);
+//	    	job.poolRequest();
+//	    	job.transformToJsonObject();
+		}
+}
+
---
+++ AntUp/src/com/antup/train12306/job/CrawlTaoBao.java
@@ -0,0 +1,221 @@
+package com.antup.train12306.job;
+
+import java.io.InputStream;  
+import java.io.StringWriter;  
+import org.apache.commons.io.IOUtils;  
+import org.apache.commons.lang.StringUtils;  
+import org.apache.http.HttpEntity;  
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.NTCredentials;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+
+public class CrawlTaoBao {
+    //模拟UA  
+    public static String UA = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.28 (KHTML, like Gecko) Chrome/26.0.1397.2 Safari/537.28";  
+
+    //中国的IP段，如果你想爬其他的IP段，加在这里就ok了�?
+    public static int[][] ip_ = { { 1, 12, 0, 0, 1, 15, 255, 255 },
+            { 1, 24, 0, 0, 1, 31, 255, 255 },  
+            { 27, 8, 0, 0, 27, 31, 255, 255 },  
+            { 27, 36, 0, 0, 27, 47, 255, 255 },  
+            { 27, 50, 128, 0, 27, 50, 255, 255 },  
+            { 27, 54, 192, 0, 27, 54, 255, 255 },  
+            { 27, 128, 0, 0, 27, 129, 255, 255 },  
+            { 27, 144, 0, 0, 27, 144, 255, 255 },
+            { 27, 148, 0, 0, 27, 159, 255, 255 },  
+            { 27, 184, 0, 0, 27, 227, 255, 255 },  
+            { 58, 14, 0, 0, 58, 25, 255, 255 },  
+            { 58, 30, 0, 0, 58, 63, 255, 255 },  
+            { 58, 66, 0, 0, 58, 67, 255, 255 },  
+            { 58, 68, 128, 0, 58, 68, 255, 255 },  
+            { 58, 82, 0, 0, 58, 83, 255, 255 },  
+            { 58, 87, 64, 0, 58, 87, 127, 255 },  
+            { 58, 99, 128, 0, 58, 101, 255, 255 },  
+            { 58, 116, 0, 0, 58, 119, 255, 255 },  
+            { 58, 128, 0, 0, 58, 135, 255, 255 },  
+            { 58, 144, 0, 0, 58, 144, 255, 255 },  
+            { 58, 154, 0, 0, 58, 155, 255, 255 },  
+            { 58, 192, 0, 0, 58, 223, 255, 255 },  
+            { 58, 240, 0, 0, 58, 255, 255, 255 },  
+            { 59, 32, 0, 0, 59, 83, 255, 255 },  
+            { 59, 107, 0, 0, 59, 111, 255, 255 },  
+            { 59, 151, 0, 0, 59, 151, 127, 255 },  
+            { 59, 155, 0, 0, 59, 155, 255, 255 },  
+            { 59, 172, 0, 0, 59, 175, 255, 255 },  
+            { 59, 191, 0, 0, 59, 255, 255, 255 },  
+            { 60, 0, 0, 0, 60, 31, 255, 255 },  
+            { 60, 55, 0, 0, 60, 55, 255, 255 },  
+            { 60, 63, 0, 0, 60, 63, 255, 255 },  
+            { 60, 160, 0, 0, 60, 191, 255, 255 },  
+            { 60, 194, 0, 0, 60, 195, 255, 255 },  
+            { 60, 200, 0, 0, 60, 223, 255, 255 },  
+            { 60, 232, 0, 0, 60, 233, 255, 255 },  
+            { 60, 235, 0, 0, 60, 235, 255, 255 },  
+            { 60, 245, 128, 0, 60, 245, 255, 255 },  
+            { 60, 247, 0, 0, 60, 247, 255, 255 },  
+            { 60, 252, 0, 0, 60, 252, 255, 255 },  
+            { 60, 253, 128, 0, 60, 253, 255, 255 },  
+            { 60, 255, 0, 0, 60, 255, 255, 255 },  
+            { 61, 4, 80, 0, 61, 4, 95, 255 },  
+            { 61, 4, 176, 0, 61, 4, 191, 255 },  
+            { 61, 8, 160, 0, 61, 8, 175, 255 },  
+            { 61, 28, 0, 0, 61, 28, 127, 255 },  
+            { 61, 29, 128, 0, 61, 29, 255, 255 },  
+            { 61, 45, 128, 0, 61, 45, 191, 255 },  
+            { 61, 47, 128, 0, 61, 47, 191, 255 },  
+            { 61, 48, 0, 0, 61, 55, 255, 255 },  
+            { 61, 87, 192, 0, 61, 87, 255, 255 },  
+            { 61, 128, 0, 0, 61, 191, 255, 255 },  
+            { 61, 232, 0, 0, 61, 237, 255, 255 },  
+            { 61, 240, 0, 0, 61, 243, 255, 255 },  
+            { 110, 6, 0, 0, 110, 7, 255, 255 },  
+            { 110, 16, 0, 0, 110, 19, 255, 255 },  
+            { 110, 40, 0, 0, 110, 43, 255, 255 },  
+            { 110, 48, 0, 0, 110, 48, 255, 255 },  
+            { 110, 51, 0, 0, 110, 53, 255, 255 },  
+            { 110, 56, 0, 0, 110, 65, 255, 255 },  
+            { 110, 72, 0, 0, 110, 73, 255, 255 },  
+            { 110, 75, 0, 0, 110, 76, 63, 255 },  
+            { 110, 76, 192, 0, 110, 77, 127, 255 },  
+            { 110, 80, 0, 0, 110, 91, 255, 255 },  
+            { 110, 94, 0, 0, 110, 127, 255, 255 },  
+            { 110, 152, 0, 0, 110, 157, 255, 255 },  
+            { 110, 166, 0, 0, 110, 167, 255, 255 },  
+            { 110, 172, 192, 0, 110, 173, 47, 255 },  
+            { 110, 173, 64, 0, 110, 173, 127, 255 },  
+            { 110, 173, 192, 0, 110, 173, 223, 255 },  
+            { 110, 176, 0, 0, 110, 223, 255, 255 },  
+            { 110, 228, 0, 0, 110, 231, 255, 255 },  
+            { 110, 232, 32, 0, 110, 232, 63, 255 },  
+            { 110, 236, 0, 0, 110, 237, 255, 255 },  
+            { 110, 240, 0, 0, 110, 255, 255, 255 },  
+            { 111, 0, 0, 0, 111, 63, 255, 255 },  
+            { 111, 66, 0, 0, 111, 66, 255, 255 },  
+            { 111, 67, 192, 0, 111, 67, 207, 255 },  
+            { 111, 68, 64, 0, 111, 68, 95, 255 },  
+            { 111, 72, 0, 0, 111, 79, 255, 255 },  
+            { 111, 85, 0, 0, 111, 85, 255, 255 },  
+            { 111, 91, 192, 0, 111, 91, 223, 255 },  
+            { 111, 112, 0, 0, 111, 117, 255, 255 },  
+            { 111, 119, 64, 0, 111, 119, 159, 255 },  
+            { 111, 120, 0, 0, 111, 124, 255, 255 },  
+            { 111, 126, 0, 0, 111, 167, 255, 255 },  
+            { 111, 170, 0, 0, 111, 170, 255, 255 },  
+            { 111, 172, 0, 0, 111, 183, 255, 255 },  
+            { 111, 186, 0, 0, 111, 187, 255, 255 },  
+            { 111, 192, 0, 0, 111, 215, 255, 255 },  
+            { 111, 221, 128, 0, 111, 222, 255, 255 },  
+            { 111, 223, 240, 0, 111, 223, 243, 255 },  
+            { 111, 223, 248, 0, 111, 223, 251, 255 },  
+            { 111, 224, 0, 0, 111, 231, 255, 255 },  
+            { 111, 235, 96, 0, 111, 235, 127, 255 },  
+            { 111, 235, 160, 0, 111, 235, 191, 255 },  
+            { 112, 0, 0, 0, 112, 67, 255, 255 },  
+            { 112, 73, 0, 0, 112, 75, 255, 255 },  
+            { 112, 80, 0, 0, 112, 87, 255, 255 },  
+            { 112, 109, 128, 0, 112, 109, 255, 255 }}; 
+    
+	private static final String PROXY_HOST= "10.18.8.108";
+	private static final int PROXY_PORT = 8008;
+	private static final String PROXY_USERNAME= "niyong";
+	private static final String PROXY_PASSWORD= "nY111111";
+	private static final String PROXY_WORKSTATION= "isa06";
+	private static final String PROXY_DOMAIN= "ulic";
+	
+    public static DefaultHttpClient getHttpClient(DefaultHttpClient httpClient){
+        NTCredentials credentials = new NTCredentials(PROXY_USERNAME ,PROXY_PASSWORD , PROXY_WORKSTATION, PROXY_DOMAIN);	        
+        httpClient.getCredentialsProvider().setCredentials(new AuthScope(PROXY_HOST, PROXY_PORT), credentials);	      
+        HttpHost proxy = new HttpHost(PROXY_HOST, PROXY_PORT);
+        httpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
+        return httpClient;
+    }
+    
+    public static void main(String[] args) {
+        try {
+            DefaultHttpClient hc = getHttpClient(new DefaultHttpClient());
+            InputStream is = null;
+            //循环上面的IP段数�?
+            for (int m = 0; m < ip_.length; m++)
+                for (int n = 0; n < ip_[0].length; n++)
+                    //循环IP段开始爬去数�? 
+                    for (int i = ip_[m][0]; i <= ip_[m][4]; i++)  
+                        for (int ii = ip_[m][1]; ii <= ip_[m][5]; ii++)  
+                            for (int iii = ip_[m][2]; iii <= ip_[m][6]; iii++)  
+                                for (int iiii = ip_[m][3]; iiii <= ip_[m][7]; iiii++) {  
+                                    HttpGet http_get = new HttpGet("http://ip.taobao.com/service/getIpInfo.php?ip="+ i + "." + ii + "." + iii + "." + iiii);
+                                    http_get.setHeader("User-Agent", UA);
+                                    IPAddr ip = new IPAddr();
+                                    int count = 0;
+                                    do {
+                                        count++;
+                                        try {
+                                            HttpEntity e = hc.execute(http_get).getEntity();
+                                            is = e.getContent();
+                                            StringWriter sw = new StringWriter();
+                                            IOUtils.copy(is, sw);
+                                            is.close();
+                                            String res = sw.toString();
+                                            System.out.println(res);
+                                            JSONObject resp = new JSONObject(res); 
+                                            if (resp.has("data")) {
+                                                JSONObject data = resp.getJSONObject("data");
+                                                if (data.has("ip")) {
+                                                    String rip = data.getString("ip");
+                                                    ip.setIp(rip);
+                                                }
+                                                if (data.has("country")) {
+                                                    ip.setCountry(data.getString("country"));
+                                                }
+                                                if (data.has("country_id")) {
+                                                    ip.setCountry_id(data.getString("country_id"));
+                                                }
+                                                if (data.has("area")) {
+                                                	ip.setArea(data.getString("area"));
+                                                } 
+                                                if (data.has("area_id")) {
+                                                    ip.setArea_id(data.getString("area_id"));
+                                                }  
+                                                if (data.has("region")) {
+                                                    ip.setRegion(data.getString("region"));  
+                                                }
+                                                if (data.has("region_id")) {  
+                                                    ip.setRegion_id(data.getString("region_id"));  
+                                                }  
+                                                if (data.has("city")) {  
+                                                	ip.setCity(data.getString("city"));                                                    
+                                                }  
+                                                if (data.has("city_id")) {
+                                                    ip.setCity_id(data.getString("city_id"));
+                                                }
+                                                if (data.has("county")) {
+                                                    ip.setCounty(data.getString("county"));
+                                                }
+                                                if (data.has("county_id")) {
+                                                    ip.setCounty_id(data.getString("county_id"));  
+                                                }  
+                                                if (data.has("isp_id")) {  
+                                                    ip.setIsp_id(data.getString("isp_id"));  
+                                                }
+                                                if (data.has("isp")) {
+                                                    ip.setIsp(data.getString("isp"));  
+                                                }  
+                                            }  
+                                        } catch (Exception e) {  
+                                            e.printStackTrace();  
+                                        }  
+                                        //因为淘宝对每秒的访问次数有限制，这里尝试十次，如果还拿不到城市，就跳过�? 
+                                    } while (StringUtils.isBlank(ip.getCity()) && count<10);
+                                    //這裡的Save()就是保存在数据库�?
+                                    System.out.println("IP地址: " + ip.getIp()                		+ "  ---- city   -----" + ip.getCity() + "  ---serialno---  "
+                                            + ip.Save());count = 0;
+                                }  
+        } catch (Exception e) {
+            e.printStackTrace();  
+        }  
+    }  
+}
